a3f8d0bb5c9a1c0ae16b734db7772be85017ebcc
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-09 13:07:43 +0100

    The  get post by id endpoint

--- router/handlers.js
+++ router/handlers.js
@@ -12,6 +12,27 @@ async function getAllPosts(req, res) {
   }
 }
 
+async function getPostById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const post = await Posts.findById(id);
+
+    if (!post.length) {
+      return res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+
+    res.status(200).json(post);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
+}
+
 module.exports = {
   getAllPosts,
+  getPostById,
 };
--- router/index.js
+++ router/index.js
@@ -3,5 +3,6 @@ const postHandler = require('express').Router();
 const handlers = require('./handlers');
 
 postHandler.get('/', handlers.getAllPosts);
+postHandler.get('/:id', handlers.getPostById);
 
 module.exports = postHandler;
